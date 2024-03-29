b685abf06158dc788024c2d1acdf9bf6d5bbfeff
MOHAMMAD KHUBAIB SARKER <mohammad@kali.kali>
2024-03-29 14:54:59 +0600

    setup project with react.js, tailwindcss, daisyUI, set router

---
+++ src/Router/Router.jsx
@@ -0,0 +1,10 @@
+import { createBrowserRouter } from "react-router-dom";
+
+const route = createBrowserRouter([
+    {
+        path: "/",
+        element: <div>this root</div>,
+    },
+]);
+
+export default route;
